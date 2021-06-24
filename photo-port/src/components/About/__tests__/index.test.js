0855d1f845f714008bcd8457908153485b356c2c
djkaz <justbekazu@gmail.com>
2021-06-24 17:39:38 -0500

    testing one

---
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+
+const { asFragment } = render(<About />);
+
+describe("About component", () => {
+  // First Test
+  it("renders", () => {
+    render(<About />);
+  });
+
+  // Second Test
+  it("matches snapshot DOM node structure", () => {
+    // render About
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+afterEach(cleanup);
